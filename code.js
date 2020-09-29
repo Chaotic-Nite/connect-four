79d8bfff38776c3c4870c327a74cd87ab0a1f1dc
kenzieadmin <kenzieadmin@users.noreply.github.com>
2020-09-29 21:37:40 +0000

    deploy: ace93b07613efe89201880c5de3b74186e1180bf

--- code.js
+++ code.js
@@ -1,1 +1,104 @@
-// Your Code Here!
+/*    
+    *Track the tokens
+    -If they reach 42
+    -More than 6 in the col
+    
+    *Place tokens
+    -insert the token into the grid
+    -find the smallest value in col
+    -Token's array location goes into a player's array
+    
+    !gameChecks - mini Functions
+    -Determines if there's is four in a row
+    -Potentially flood fill ali
+    -Checks selectedPlayer Array if there's any connected 4
+    -if the token placed is in the center, is that needs to check 1 above
+    
+    clickHandlers*
+        
+        [1]2
+        
+        1 [ 0, 1, 2, 3, 4, 5 ]
+        2 [ 0, 1, 2, 3, 4, 5 ]
+        3 [ 0, 1, 2, 3, 4, 5 ]
+        4 [ 0, 1, 2, 3, 4, 5 ]
+        5 [ 0, 1, 2, 3, 4, 5 ]
+        */
+
+let selectedPlayer = "red";
+
+let redPlayer = [],
+  blackPlayer = [],
+  playerArr = [],
+  boardArr = [];
+
+function twoArray() {
+  let arr = new Array(7);
+  for (let i = 0; i < 7; i++) {
+    arr[i] = new Array(6);
+  }
+
+  return arr;
+}
+
+function drawBoard() {
+  let gameDiv = document.getElementById("board");
+
+  boardArr = twoArray();
+
+  for (let row = 0; row < boardArr.length; row++) {
+    let gameRow = document.createElement("div");
+    for (let col = 0; col < 6; col++) {
+      let cell = document.createElement("div");
+      cell.classList.add("token");
+      cell.classList.add("col" + row);
+
+      gameRow.append(cell);
+    }
+    gameDiv.append(gameRow);
+  }
+}
+
+function playerToggle() {
+  if (selectedPlayer === "red") {
+    redPlayer = playerArr;
+    selectedPlayer = "black";
+    playerArr = blackPlayer;
+  } else if (selectedPlayer === "black") {
+    blackPlayer = playerArr;
+    selectedPlayer = "red";
+    playerArr = redPlayer;
+  }
+}
+
+/* 
+
+playerArr = [i, j, k]
+
+redArr => playArr = [i, j, k]
+
+playerArr => blackArr = []
+
+*/
+
+function clickHandler(event) {}
+
+function initializer() {
+  drawBoard();
+
+  document.querySelector(".col0").addEventListener("click", clickHandler);
+
+  document.querySelector(".col1").addEventListener("click", clickHandler);
+
+  document.querySelector(".col2").addEventListener("click", clickHandler);
+
+  document.querySelector(".col3").addEventListener("click", clickHandler);
+
+  document.querySelector(".col4").addEventListener("click", clickHandler);
+
+  document.querySelector(".col5").addEventListener("click", clickHandler);
+
+  document.querySelector(".col6").addEventListener("click", clickHandler);
+}
+
+initializer();
--- readme.md
+++ readme.md
@@ -1,3 +1,18 @@
 # Connect Four
 
 Follow the Instructions on my.kenzie.academy
+
+# Development Plan
+
+1. Visiable game board -> Bri
+   1. Create Div to contain the board
+   2. border-radius on the tokens
+2. Same system 2 players
+   1. Set the main player at the beginning
+      1. Boolean to altenate between (Can be done in a function)
+   2. Selects a col takes highest position
+      1. Create 2D array
+      2. Assign cols as classes
+   3. Stores where the player token sits
+      1. Global variable for individual players
+3. Connect four wins
